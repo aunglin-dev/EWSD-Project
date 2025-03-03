756c087165b8085d6026621ecdd6e214e429b572
ktk <ktkthirikyaw@gmail.com>
2025-03-04 00:32:32 +0630

    fix responsiveness

--- frontend/src/components/allocateProcess/allocate-form.js
+++ frontend/src/components/allocateProcess/allocate-form.js
@@ -161,7 +161,7 @@ export default function AllocateForm() {
       paddingY="100px"
       paddingX={isNonMobileScreens ? "20px" : "10px"}
     >
-      <Box display="flex" justifyContent="start" alignItems="center" gap="10px">
+      <Box display="flex" flexDirection={isSmallestScreens && "column"} justifyContent="start" alignItems={isSmallestScreens ? "start" : "center"} gap="10px">
         <Button href="/allocate" type="button" variant="text" sx={{ padding: 0, fontSize: "16px", display: "flex", justifyContent: "space-between", alignItems: "center", gap: "5px" }}>
           {/* <span
           style={{
@@ -178,7 +178,7 @@ export default function AllocateForm() {
           {/* </span> */}
         </Button>
         {successMsg && (
-          <span style={{ fontSize: "14px", fontWeight: "500", marginLeft: "30px", padding: "5px 25px", borderRadius: "20px", backgroundColor: "rgba(0, 200, 0, 0.4)", }}>
+          <span style={{ fontSize: "14px", fontWeight: "500", marginLeft: isSmallestScreens ? "0px" : "30px", padding: "5px 25px", borderRadius: "20px", backgroundColor: "rgba(0, 200, 0, 0.4)", }}>
             {successMsg}
           </span>
         )}
--- frontend/src/components/allocateProcess/allocate-reallocate.js
+++ frontend/src/components/allocateProcess/allocate-reallocate.js
@@ -155,7 +155,7 @@ export default function AllocateReallocate() {
             paddingY="100px"
             paddingX={isNonMobileScreens ? "20px" : "10px"}
         >
-            <Box display="flex" justifyContent="start" alignItems="center" gap="10px">
+            <Box display="flex" flexDirection={isSmallestScreens && "column"} justifyContent="start" alignItems={isSmallestScreens ? "start" : "center"} gap="10px">
                 <Button href="/allocate" type="button" variant="text" sx={{ padding: 0, fontSize: "16px", display: "flex", justifyContent: "space-between", alignItems: "center", gap: "5px" }}>
                     {/* <span
           style={{
@@ -171,8 +171,8 @@ export default function AllocateReallocate() {
                     <KeyboardBackspaceIcon sx={{ width: "18px", height: "18px" }} /> Back
                     {/* </span> */}
                 </Button>
-                {!loading && successMsg && (
-                    <span style={{ fontSize: "14px", fontWeight: "500", marginLeft: "30px", padding: "5px 25px", borderRadius: "20px", backgroundColor: "rgba(0, 200, 0, 0.4)", }}>
+                {successMsg && (
+                    <span style={{ fontSize: "14px", fontWeight: "500", marginLeft: isSmallestScreens ? "0px" : "30px", padding: "5px 25px", borderRadius: "20px", backgroundColor: "rgba(0, 200, 0, 0.4)", }}>
                         {successMsg}
                     </span>
                 )}
