0b1d10810978358a8ff72d3cb87cca5daa5a556e
aungkhant777 <aungkhant.k389336@gmail.com>
2025-04-29 22:39:16 +0630

    adjust offline online

--- frontend/src/components/meetingProcess/student-meetings.js
+++ frontend/src/components/meetingProcess/student-meetings.js
@@ -207,7 +207,7 @@ export default function StudentMeetings() {
                 key={meeting._id}
                 meetingId={meeting._id}
                 title={meeting.title}
-                type={meeting.type === "virtual" ? "online" : "offline"}
+                type={meeting.type === "online" ? "online" : "offline"}
                 datetime={new Date(meeting.dateTime).toLocaleString()}
                 remark={meeting.remark || ""}
                 declined={meeting.status === 2}
